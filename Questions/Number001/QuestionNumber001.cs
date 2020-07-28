adb62eb834be225a479653e528f894da3844b950
ÜMİT KARABACAK EV <umit.karabacak@hotmail.com.tr>
2020-07-29 01:57:06 +0300

    #Questions/Number001
    001 Numaralı soru için gerekli olan detaylar oluşturulmuştur.

---
+++ Questions/Number001/QuestionNumber001.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Web.Mvc;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Dynamic;
+
+namespace Questions.Number001
+{
+    public enum EnumDeviceType
+    {
+        [Description("IOS")] IOS = 1,
+        [Description("Android")] Android = 2,
+        [Description("Web")] Web = 3,
+    }
+
+    public class UserRegisterDetail
+    {
+        public EnumDeviceType EnumDeviceType { get; set; }
+        public DateTime? RegisteredDate { get; set; }
+    }
+
+    public static class StaticData
+    {
+        public static List<UserRegisterDetail> SampleData()
+        {
+            var sampleData = new List<UserRegisterDetail>()
+            {
+                new UserRegisterDetail { RegisteredDate = new DateTime(2020,7,28), EnumDeviceType = EnumDeviceType.Web },
+                new UserRegisterDetail { RegisteredDate = new DateTime(2020,7,30), EnumDeviceType = EnumDeviceType.IOS },
+                new UserRegisterDetail { RegisteredDate = new DateTime(2020,7,29), EnumDeviceType = EnumDeviceType.Android },
+                new UserRegisterDetail { RegisteredDate = new DateTime(2020,7,29), EnumDeviceType = EnumDeviceType.IOS },
+                new UserRegisterDetail { RegisteredDate = new DateTime(2020,7,30), EnumDeviceType = EnumDeviceType.IOS },
+                new UserRegisterDetail { RegisteredDate = new DateTime(2020,7,29), EnumDeviceType = EnumDeviceType.Android },
+                new UserRegisterDetail { RegisteredDate = new DateTime(2020,7,28), EnumDeviceType = EnumDeviceType.Android},
+                new UserRegisterDetail { RegisteredDate = new DateTime(2020,7,29), EnumDeviceType = EnumDeviceType.Android },
+                new UserRegisterDetail { RegisteredDate = new DateTime(2020,7,28), EnumDeviceType = EnumDeviceType.Android },
+                new UserRegisterDetail { RegisteredDate = new DateTime(2020,7,29), EnumDeviceType = EnumDeviceType.Android },
+                new UserRegisterDetail { RegisteredDate = new DateTime(2020,7,30), EnumDeviceType = EnumDeviceType.IOS },
+                new UserRegisterDetail { RegisteredDate = new DateTime(2020,7,30), EnumDeviceType = EnumDeviceType.Android },
+                new UserRegisterDetail { RegisteredDate = new DateTime(2020,7,31), EnumDeviceType = EnumDeviceType.Android },
+                new UserRegisterDetail { RegisteredDate = new DateTime(2020,7,30), EnumDeviceType = EnumDeviceType.IOS },
+                new UserRegisterDetail { RegisteredDate = new DateTime(2020,7,31), EnumDeviceType = EnumDeviceType.Web },
+                new UserRegisterDetail { RegisteredDate = new DateTime(2020,7,31), EnumDeviceType = EnumDeviceType.IOS },
+                new UserRegisterDetail { RegisteredDate = null, EnumDeviceType = EnumDeviceType.IOS },
+                new UserRegisterDetail { RegisteredDate = new DateTime(2020,7,27), EnumDeviceType = EnumDeviceType.IOS }
+            };
+
+            return sampleData;
+        }
+    }
+
+    public class RequestUserRegisterReport
+    {
+        public DateTime? ReportStartDate { get; set; }
+        public DateTime? ReportEndDate { get; set; }
+    }
+
+    public class ResponseReportSummary
+    {
+        public string Name { get; set; }
+        public int Value { get; set; }
+    }
+
+    public class ResponseUserReportRegister
+    {
+        public string Name { get; set; }
+        public List<ResponseReportSummary> Series { get; set; }
+    }
+
+    public class ResponseRegisterReport
+    {
+        public List<ResponseUserReportRegister> Series { get; set; }
+    }
+
+    public class ResponseController : Controller
+    {
+        public ResponseRegisterReport GetUserReport()
+        {
+            return null;
+        }
+    }
+}
---
+++ Questions/Number001/Readme.md
@@ -0,0 +1,26 @@
+﻿# This Question Number 001 Readme File
+
+QuestionNumber001 için 
+
+	UserRegisterDetail sisteme kayıt olan kullanıcıların kayıt olduğu detay bilgilerini tutmaktadır.
+
+	EnumDeviceType kayıt aşamasında kullandıkları cihaz bilgisini göstermektedir.
+
+	StaticData örnek olarak kullanılabilmesi için verilmiştir, daha fazla yada daha az kayıtlarla işlem yapılabilir.
+
+	RequestUserRegisterReport oluşturulacak olan rapor üzerinde girilen tarih aralığına göre filtreleme işlemi yapmak için kullanılacaktır.
+
+	ResponseReportSummary oluşturulacak olan raporda bir günlük cihaz tipini ve kayıt adedini tutmak için kullanılacak dönüş objesidir.
+	
+	ResponseUserReportRegister oluşturulacak olan raporda "Android", "IOS" gibi kırılımları yapabilmek için oluşturulmuş bir dönüş objesidir.
+
+	ResponseRegisterReport raporu isteyen kullanıcıya dönecek olan listenin tamamıdır.
+
+	ResponseController içerisinde ki örnek GetUserReport fonksiyonu yol göstermesi için oluşturulmuştur;
+		İstek yapılan türün nasıl olacağı 
+		Geri dönüş tipinin nasıl olacağı 
+
+Ve son olarak QuestionNumber001SampleDataResponse dosyasında örnek çıktı gösterilmiştir (herhangi bir filtre uygulanmamıştır.)
+
+Daha fazla detay görüşme sonrası verilecektir,
+Çözecek arkadaşlara şimdiden başarılar diler ve hatırlatırım bana geri dönüş yapmayı unutmayın (:
