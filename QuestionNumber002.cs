f014b9ce959a191663c3d753d083710c774e105c
ÜMİT KARABACAK <umit.karabacak@hotmail.com.tr>
2020-08-21 01:00:28 +0300

    #Questions/Number002
    002 Numaralı soru için gerekli olan detaylar oluşturulmuştur.

---
+++ QuestionNumber002.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic
+
+namespace Questions.Number002
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string FullName => $"{Id} -> {Name}";
+    }
+
+    public class EmployeeManager001
+    {
+        public List<Employee> GetEmployeesMethodOne()
+        {
+            List<Employee> employees = new List<Employee>();
+            for (int i = 0; i < 10; i++)
+            {
+                employees.Add(new Employee
+                {
+                    Id = i,
+                    Name = $"Example User Name{i}"
+                });
+            }
+        }
+    }
+
+    public class EmployeeManager002
+    {
+        public List<Employee> GetEmployeesMethodTwo()
+        {
+            List<Employee> employees = new List<Employee>();
+            for (int i = 0; i < 10; i++)
+            {
+                Employee employee = new Employee();
+                employee.Id = i;
+                employee.Name = $"Example User Name{i}";
+
+                employees.Add(employee);
+            }
+        }
+    }
+    public class EmployeeManager003
+    {
+        public List<Employee> GetEmployeesMethodThree()
+        {
+            List<Employee> employees = new List<Employee>();
+            for (int i = 0; i < 10; i++)
+            {
+                Employee employee = new Employee()
+                {
+                    Id = i,
+                    Name = $"Example User Name{i}"
+                };
+
+                employees.Add(employee);
+            }
+        }
+    }
+
+    public class Program
+    {
+        void Main(string[] args)
+        {
+            var usageOne = new EmployeeManager001().GetEmployeesMethodOne();
+            var usageTwo = new EmployeeManager002().GetEmployeesMethodTwo();
+            var usageThree = new EmployeeManager003().GetEmployeesMethodThree();
+        }
+    }
+}
---
+++ Questions/Number002/Readme.md
@@ -0,0 +1,15 @@
+# This Question Number 002 Readme File
+
+QuestionNumber002 için 
+
+	Employee proje içerisinde kullanýlacak herhangi bir þeyi temsil etmektedir.
+
+	EmployeeManager001 ise sabit olarak yaratýlan 10 kiþilik bir personel listesi oluþturmaktadýr. Tanýmlanacak personel direkt olarak listeye eklemek aþamasýnda oluþturulmuþtur.
+	EmployeeManager002 ise sabit olarak yaratýlan 10 kiþilik bir personel listesi oluþturmaktadýr. Oluþturulan sabit listesi üzerinde döngü devam ederken önce personel oluþturulmuþ, daha sonra listeye ekleme iþlemi yapýlmýþtýr.
+
+	Program sýnýfýnda ise Test için her sýnýfda kullanýlmýþtýr.
+
+Sorular
+	Hangi yöntemde bellek tüketimi daha fazla olmuþtur, neden?
+	2. Yöntem üzerinde oluþturulan `employee` 'ýn kaç referansý oluþmuþtur.
+	2. ve 3. Yöntem arasýnda herhangi bir performans farký varmýdýr, neden ?
